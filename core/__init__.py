d856cd919a15792b1d33a1ef71e85f0923d51cc9
Gabriel Tenita <the.ge@tenita.eu>
2024-03-11 14:16:39 +0200

    Replaced another f-string

--- core/__init__.py
+++ core/__init__.py
@@ -4,4 +4,4 @@ import sublime
 
 min_version = 4085
 if int(sublime.version()) < min_version:
-    raise RuntimeError(f'Shelf works with Sublime Text build {min_version}+ only.')
+    raise RuntimeError('Shelf works with Sublime Text build ' + str(min_version) + ' only.')
