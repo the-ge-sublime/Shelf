f4ef8f995dd1094b7601bf946e2977826bfbc86c
Gabriel Tenita <the.ge@tenita.eu>
2024-03-11 14:49:09 +0200

    Replaced all the f-strings

--- core/renderer.py
+++ core/renderer.py
@@ -15,24 +15,39 @@ class Renderer:
         # by calculating the file names column length using a totally magic factor of 0.6
         # it will probably fail with wider character fonts
         name_rems = round(max(len(prject_max_name), len(common_max_name)) * 0.6)
-        css = f'<style>\n{sublime.load_resource("Packages/Shelf/assets/css/shelf.css").strip()}\n.main {{ width: {name_rems}rem; }}\n</style>'
+        css = (
+            '<style>\n'
+            + sublime.load_resource('Packages/Shelf/assets/css/shelf.css').strip()
+            + '\n'
+            + '  .main {\n'
+            + '    width: '
+            + str(name_rems)
+            + 'rem;\n'
+            + '  }\n'
+            + '</style>\n'
+        )
 
-        return f"""\n<body id="shelf-popup">\n{css}
-          <div class="close">
-            <a href="#" class="close-btn btn"><img class="btn-icon" src="res://Packages/Shelf/assets/img/close-{self.color_scheme}.png"></a>
-          </div>
-          <h3 class="header">
-            <span>PROJECT</span>
-          </h3>
-          <div class="table">
-            {prject_shelf}
-          </div>
-          <h3 class="header">COMMON</h3>
-          <div class="table">
-            {common_shelf}
-          </div>
-        </body>
-        """
+        return (
+            '\n'
+            + '<body id="shelf-popup">\n'
+            + css
+            + '    <div class="close">\n'
+            + '        <a href="#" class="close-btn btn"><img class="btn-icon" src="res://Packages/Shelf/assets/img/close-'
+            + self.color_scheme
+            + '.png"></a>'
+            + '    </div>\n'
+            + '    <h3 class="header">\n'
+            + '        <span>PROJECT</span>\n'
+            + '    </h3>\n'
+            + '    <div class="table">\n'
+            + prject_shelf
+            + '    </div>\n'
+            + '    <h3 class="header">COMMON</h3>\n'
+            + '    <div class="table">\n'
+            + common_shelf
+            + '    </div>\n'
+            + '    </body>'
+        )
 
     def render_shelf(self, shelf):
         items = shelf.read()
@@ -42,17 +57,17 @@ class Renderer:
             alt_class = 'tr-' + ('odd' if is_odd else 'even')
             open_file_action = self.render_action(
                 name='open_file',
-                args={'file': path, 'content': f'Could not open {path}'},
+                args={'file': path, 'content': 'Could not open ' + path},
                 text=name,
-                clss ='btn text-btn',
-                title=f'Open {path}'
+                clss='btn text-btn',
+                title='Open ' + path,
             )
-            rendered += f"""
-                <div class="tr {alt_class} tbody">
-                    <h4 class="td main">{open_file_action}</h4>
-                    <div class="td aside">{self.side_actions(item, shelf.key)}</div>
-                </div>
-            """
+            rendered += (
+                '        <div class="tr ' + alt_class + ' tbody">\n'
+                + '            <h4 class="td main">' + open_file_action + '</h4>\n'
+                + '            <div class="td aside">' + self.side_actions(item, shelf.key) + '</div>\n'
+                + '        </div>\n'
+            )
             max_name = max_name if max(len(max_name), len(name)) == len(max_name) else name
             is_odd = not is_odd
             k += 1
@@ -61,7 +76,7 @@ class Renderer:
 
     def side_actions(self, item, shelf):
         args = {'item': item, 'shelf': shelf}
-        clss='btn action-btn'
+        clss = 'btn action-btn'
         _, path = item
         containing_folder_path = os.path.dirname(path)
         side_actions = self.render_action(
@@ -69,7 +84,7 @@ class Renderer:
             {'dir': containing_folder_path},
             self.icon('folder'),
             clss,
-            f'Open containing folder ({containing_folder_path})'
+            'Open containing folder (' + containing_folder_path + ')',
         )
         actions = [
             ('shelf_item_move_up', self.icon('arrow-up'), 'Move up'),
@@ -77,11 +92,11 @@ class Renderer:
             ('shelf_item_remove', self.icon('trash'), 'Remove'),
         ]
 
-        return side_actions + "".join([self.render_action(name, args, text, clss, title) for (name, text, title) in actions])
+        return side_actions + ''.join([self.render_action(name, args, text, clss, title) for (name, text, title) in actions])
 
     def icon(self, icon_name):
-        return f'<img class="btn-icon" src="res://Packages/Shelf/assets/img/{icon_name}-{self.color_scheme}.png">'
+        return '<img class="btn-icon" src="res://Packages/Shelf/assets/img/' + icon_name + '-' + self.color_scheme + '.png">'
 
     def render_action(self, name, args, text, clss, title):
         href = 'subl:' + sublime.html_format_command(name, args)
-        return f'<a href="{href}" class="{clss}" title="{title}">{text}</a>'
+        return '<a href="' + href + '" class="' + clss + '" title="' + title + '">' + text + '</a>'
