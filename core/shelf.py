523486707bd5fb976eee8b90e3049b04a453e20e
Gabriel Tenita <the.ge@tenita.eu>
2024-03-30 15:57:23 +0200

    Added UTF-8 encoding arg to open calls

--- core/shelf.py
+++ core/shelf.py
@@ -13,19 +13,19 @@ class Shelf():
 
     def clear(self):
         if self.file:
-            with open(self.file, 'w'):
+            with open(self.file, 'w', encoding='utf_8'):
                 pass
 
     def read(self):
         items = ()
         if self.file and os.path.exists(self.file):
-            with open(self.file, 'r', newline='') as f:
+            with open(self.file, 'r', encoding='utf_8', newline='') as f:
                 items = tuple(tuple(row) for row in csv.reader(f))
 
         return items
 
     def write(self, items):
-        with open(self.file, 'w+', newline='') as f:
+        with open(self.file, 'w+', encoding='utf_8', newline='') as f:
             writer = csv.writer(f)
             writer.writerows(items)
 
