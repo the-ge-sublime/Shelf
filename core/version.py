cd9bb6e7840b3530e3665a3172b5ca3b8c3971df
Gabriel Tenita <the.ge@tenita.eu>
2024-03-31 19:50:33 +0300

    Added core/version.py

---
+++ core/version.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# File name: version.py
+# Created:   2024-03-31 12:52
+# @author    Gabriel Tenita <dev2023@tenita.eu>
+# @link      https://github.com/the-ge/
+# @copyright Copyright (c) 2024-present Gabriel Tenita
+# @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License version 2.0
+
+
+__version__ = '0.0.3'
