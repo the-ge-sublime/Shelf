7b7b581735ec400b116253afe67786911380c3dd
the-ge <g1704578400@tenita.eu>
2026-09-02 17:05:40 +0300

    chore: add copyright notice to core/__init__.py

---
+++ core/__init__.py
@@ -0,0 +1,6 @@
+# File name: __init__.py
+# Created:   2024-03-11 09:24
+# @author    Gabriel Tenita <g1704578400@tenita.eu@tenita.eu>
+# @see       https://github.com/the-ge/
+# @copyright Copyright (c) 2026-present Gabriel Tenita
+# @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License version 2.0
