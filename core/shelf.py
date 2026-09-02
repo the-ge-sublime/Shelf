225dae10e8695ff21427d2cd64029dd92a5975bc
the-ge <g1704578400@tenita.eu>
2026-09-02 17:34:00 +0300

    chore(shelf): add copyright notice

--- core/shelf.py
+++ core/shelf.py
@@ -1,4 +1,9 @@
-# -*- coding: UTF-8 -*-
+# File name: shelf.py
+# Created:   2024-03-11 09:24
+# @author    Gabriel Tenita <g1704578400@tenita.eu@tenita.eu>
+# @see       https://github.com/the-ge/
+# @copyright Copyright (c) 2026-present Gabriel Tenita
+# @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License version 2.0
 
 import csv
 import os
