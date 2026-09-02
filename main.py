7938ce90dc77894f410f136c92c771f30b813ebe
the-ge <g1704578400@tenita.eu>
2026-09-02 17:16:49 +0300

    refactor(main): add type annotations

--- main.py
+++ main.py
@@ -8,6 +8,9 @@
 # @copyright Copyright (c) 2024-present Gabriel Tenita
 # @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License version 2.0
 
+from __future__ import annotations
+
+import sublime
 import sublime_plugin
 
 from .core.renderer import Renderer
@@ -16,7 +19,7 @@ from .core.shelf import CommonShelf, ProjectShelf
 #from Shelf.core.debug import _d
 
 
-def get_shelf(name):
+def get_shelf(name: str) -> CommonShelf | ProjectShelf:
     shelves = {
         "common": CommonShelf,
         "project": ProjectShelf,
@@ -31,19 +34,19 @@ def get_shelf(name):
 class ShelfCommand(sublime_plugin.WindowCommand):
     """Common helpers shared by all shelf commands."""
 
-    def render(self):
+    def render(self) -> str:
         view = self.window.active_view()
         return Renderer().render_shelves(view.style()["foreground"])
 
-    def refresh_popup(self):
+    def refresh_popup(self) -> None:
         self.window.active_view().update_popup(self.render())
 
 
 class ShelfViewCommand(ShelfCommand):
-    def run(self):
+    def run(self) -> None:
         self.show_popup(self.render())
 
-    def show_popup(self, content):
+    def show_popup(self, content: str) -> None:
         view = self.window.active_view()
 
         width, height = view.viewport_extent()
@@ -58,13 +61,13 @@ class ShelfViewCommand(ShelfCommand):
             # flags=sublime.KEEP_ON_SELECTION_MODIFIED,
         )
 
-    def on_navigate(self, href):
+    def on_navigate(self, href: str) -> None:
         if href == "#":
             self.window.active_view().hide_popup()
 
 
 class ShelfAddCommand(ShelfCommand):
-    def run(self, shelf):
+    def run(self, shelf: CommonShelf | ProjectShelf) -> None:
         item = self.window.active_view().file_name()
         if not item:
             return
@@ -73,18 +76,18 @@ class ShelfAddCommand(ShelfCommand):
 
 
 class ShelfItemMoveUpCommand(ShelfCommand):
-    def run(self, item, shelf):
+    def run(self, shelf: str, item: list[str, str]) -> None:
         get_shelf(shelf).move_up(tuple(item))
         self.refresh_popup()
 
 
 class ShelfItemMoveDownCommand(ShelfCommand):
-    def run(self, item, shelf):
+    def run(self, shelf: str, item: list[str, str]) -> None:
         get_shelf(shelf).move_down(tuple(item))
         self.refresh_popup()
 
 
 class ShelfItemRemoveCommand(ShelfCommand):
-    def run(self, shelf, item):
+    def run(self, shelf: str, item: list[str, str]) -> None:
         get_shelf(shelf).remove(tuple(item))
         self.refresh_popup()
